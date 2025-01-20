71a4679daa70f22e554f15674b147bfec643bb1b
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2025-01-20 23:53:33 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-    const n = a.length;   
-    let answer = 0;
-    for (let i = 0; i < n; i++) {
-        answer += a[i]*b[i];
-    }
-    return answer;
+    return a.reduce((acc, cur, i) => acc + cur*b[i], 0);
 }
