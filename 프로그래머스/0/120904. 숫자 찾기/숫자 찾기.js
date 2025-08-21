899ada20e50dcd89cb6cce780fef60ec7004bed0
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2025-08-21 14:38:28 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,7 +1,3 @@
 function solution(num, k) {
-    const candidates = [...String(num)].map((a, idx) => {if (Number(a) === k) return idx + 1});
-    for (const candidate of candidates) {
-        if (candidate !== undefined) return candidate;
-    }
-    return -1;
+    return String(num).indexOf(k) + 1 || -1;
 }
