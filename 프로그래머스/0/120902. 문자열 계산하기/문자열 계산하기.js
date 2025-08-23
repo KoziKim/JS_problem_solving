f7b3ef3903c15844aa26c9f56b9838898ee59abb
Kyungmin Kim <126449657+KoziKim@users.noreply.github.com>
2025-08-24 00:42:00 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,20 +1,20 @@
+const calculator = (x, sign, answer) => {
+    if (x === '+') {
+        sign = 1;
+    } else if (x === '-') {
+        sign = -1;
+    } else {
+        answer += sign * x;
+    }
+    
+    return [answer, sign];
+}
+
 function solution(my_string) {
     let answer = 0;
-    let sign = undefined;
+    let sign = 1;
     my_string.split(' ').forEach((x) => {
-        if (x === '+') {
-            sign = 'plus';
-        } else if (x === '-') {
-            sign = 'minus';
-        }
-        if (x === '+' || x === '-'){    
-        } else if (!sign) {
-            answer += Number(x);
-        } else if (sign === 'plus') {
-            answer += Number(x);
-        } else if (sign === 'minus') {
-            answer -= Number(x);
-        }
+        [answer, sign] = calculator(x, sign, answer);
     });
     return answer;
 }
